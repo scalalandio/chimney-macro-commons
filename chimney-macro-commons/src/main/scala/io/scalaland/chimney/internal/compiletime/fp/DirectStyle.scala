67168daa141e908e8100b21b15cc37ee94dd0400
Mateusz Kubuszok <mateusz.kubuszok@gmail.com>
2025-05-08 12:08:46 +0200

    Update Scala 3 to 3.3.6

--- chimney-macro-commons/src/main/scala/io/scalaland/chimney/internal/compiletime/fp/DirectStyle.scala
+++ chimney-macro-commons/src/main/scala/io/scalaland/chimney/internal/compiletime/fp/DirectStyle.scala
@@ -12,13 +12,15 @@ object DirectStyle {
 
   def apply[F[_]](implicit F: DirectStyle[F]): DirectStyle[F] = F
 
-  def directStyleForEither[Errors]: DirectStyle[Either[Errors, *]] = new DirectStyle[Either[Errors, *]] {
-    private case class PassErrors(error: Errors) extends NoStackTrace
+  def directStyleForEither[Errors]: DirectStyle[Either[Errors, *]] = new DirectStyle[Either[Errors, *]] { ds =>
+    private case class PassErrors(error: Errors) extends NoStackTrace {
+      val parent = ds
+    }
 
     def asyncUnsafe[A](thunk: => A): Either[Errors, A] = try
       Right(thunk)
     catch {
-      case PassErrors(error) => Left(error)
+      case err: PassErrors @unchecked if err.parent == ds => Left(err.error)
     }
     def awaitUnsafe[A](value: Either[Errors, A]): A = value match {
       case Left(error)  => throw PassErrors(error)
